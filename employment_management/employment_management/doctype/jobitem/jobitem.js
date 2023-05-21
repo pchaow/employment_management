990bef4e0bbce7607c6af49dd0db8aa3710ed467
Chaow Porkaew <pchaowmobile@gmail.com>
2023-05-21 09:59:46 +0700

    initialize project

---
+++ employment_management/employment_management/doctype/jobitem/jobitem.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Zezembly Co.,Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('JobItem', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ employment_management/employment_management/doctype/jobitem/test_jobitem.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Zezembly Co.,Ltd. and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestJobItem(FrappeTestCase):
+	pass
